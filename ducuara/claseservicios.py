4dcf6c2a379563d107426a56c7137c4b2f525e0d
sagrario <sagrariocacaris2004@gmail.com>
2023-04-08 23:12:28 -0500

    clases

---
+++ ducuara/claseservicios.py
@@ -0,0 +1,64 @@
+from servicios import *
+import sqlite3
+
+with sqlite3.connect('C:\\Ducuara1\\db\\proyectolodging\\lodging2_db.db')as con:
+    micursor=con.cursor()
+
+    class servicio:
+        def __init__(self, id_servicio, tiposervicio, valor_servicio):
+            self.id_servicio = id_servicio
+            self.tiposervicio = tiposervicio
+            self.valor_servicio = valor_servicio
+
+        def agregar_servicio(self, conexion):
+            cursor = conexion.cursor()
+            cursor.execute('INSERT INTO serviadicional VALUES ( ?, ?, ?)',
+                           (self.id_servicio, self.tiposervicio, self.valor_servicio))
+        
+            conexion.commit()
+
+        def actualizar_servicio(self, conexion,id_servicio, tiposervicio, valor_servicio):
+            cursor = conexion.cursor()
+            cursor.execute('UPDATE serviadicional SET id_servicio=?, tiposervicio=?, valor_servicio=? WHERE id_servicio=?',
+                        (id_servicio, tiposervicio, valor_servicio, id_servicio))
+            conexion.commit()
+            if cursor.rowcount > 0:
+                return "El servicio ha sido actualizado."
+            else:
+                return "No se ha encontrado el servicio con el id especificado."
+            
+
+        def cancelar_servicio(self, conexion):
+            cursor = conexion.cursor()
+            cursor.execute('DELETE FROM serviadicional WHERE id_servicio=?', (self.id_servicio,))
+            conexion.commit()
+            if cursor.rowcount > 0:
+                print('El servicio ha sido cancelado')
+            else:
+                 print('No se ha encontrado el servicio con el id especificado')
+
+
+
+        def listar(conexion):
+            cursor = conexion.cursor()
+            cursor.execute('SELECT * FROM serviadicional')
+            servicios = cursor.fetchall()
+            return [servicio(*p) for p in servicios]
+
+'''agregar_nuevo_servicio=servicio(11,'servicio de lavanderia', 15000)
+agregar_nuevo_servicio.agregar_servicio(con)
+'''
+
+'''mis_servicios = servicio(15,'bar',15000)
+nuevo_servicio='discoteca'
+nuevo_valor=150000
+mensaje = mis_servicios.actualizar_servicio(con,15, nuevo_servicio, nuevo_valor)
+print(mensaje)'''
+
+servicio_un_cancelar=servicio(id_servicio=14, tiposervicio=None, valor_servicio=None)  
+servicio_un_cancelar.cancelar_servicio(con)
+
+
+
+servicios = servicio.listar(con)
+print(servicios)
---
+++ ducuara/usuarios.py
@@ -0,0 +1,39 @@
+import sqlite3
+with sqlite3.connect('C:\\Ducuara1\\db\\proyectolodging\\lodging2_db.db')as con:
+    micursor=con.cursor()
+    sentencia="SELECT * FROM usuarios"
+    print(micursor.execute(sentencia).fetchall())
+    
+def modificar(connetc,tabla,campo,dato,id_usuario):
+    micursor=connetc.cursor()
+    sentencia=f"UPDATE {tabla} SET {campo}= '{dato}' WHERE id_usuario ='{id_usuario}'"
+    micursor.execute(sentencia)
+    con.commit()
+    print('Modificacion Exitosa')
+'''modificar(con,'usuarios','nombre_usuario','saray',10) '''
+
+def eliminar(connetc,tabla,campo,dato,id_usuario):
+    micursor=connetc.cursor()
+    sentencia=f"DELETE FROM {tabla} WHERE {campo}= '{dato}'"
+    micursor.execute(sentencia)
+    con.commit()
+    print('Eliminacion Exitosa')
+'''eliminar(con,'usuarios','nombre_usuario','saray',10) '''
+def insertar(id_usuario,nombre_usuario,apelido_usuario,direccion_usuario,telefono_usuario,email_usuario,pass_usuario,fecharest_usuario,fechareg_usuario,id_tipo_usuario):
+    micursor=con.cursor()
+    sentencia=f"INSERT INTO usuarios  VALUES ({id_usuario},'{nombre_usuario}','{apelido_usuario}','{direccion_usuario}','{telefono_usuario}','{email_usuario}','{pass_usuario}','{fecharest_usuario}','{fechareg_usuario}',{id_tipo_usuario})"
+    micursor.execute(sentencia)
+    con.commit()
+    print('Agregacion exitosa')
+
+'''insertar(10,'saray','marinez','calle 8','423525','saray@gmail.com','erwef','03/05/2023','03/04/2023','10')'''
+
+def leer():
+    micursor=con.cursor()
+    sentencia=f"SELECT * FROM usuarios"
+    micursor.execute(sentencia)
+    datos=micursor.fetchall()
+    con.commit()
+    print(datos)
+
+'''leer()'''
